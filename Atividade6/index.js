0ac08e88daf6c55a4529c32c46ee27d32f95287d
Yuri Ibrahim de Faria <yuri.faria@a.ucb.br>
2023-10-04 10:32:41 -0300

    uma hora vai

--- Atividade6/index.js
+++ Atividade6/index.js
@@ -2,9 +2,18 @@ const express =  require('express');
 const mustacheExpress =  require('mustacheExpress');
 const app = express();
 
-app.engine('html' mustacheExpress());
+app.engine('html', mustacheExpress());
 app.set('view enginer', 'html');
 app.set('views', __dirname + '/views');
 
 app.use(express.urlencoded(extended: true));
 
+app.get("/", function(req, res){
+    res.render("index.html");
+});
+
+app.post("/dados", function(req, res){
+    let agendamento = {
+        nome: req.body.nome
+    };
+});
