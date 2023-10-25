83235c2bfa3f708efea5bee32ba9db20816d55b0
Leticia-Dantas <99661825+Leticia-Dantas@users.noreply.github.com>
2023-10-24 22:09:56 -0300

    Update calculadora.js

--- Atividade8/src/calculadora/calculadora.js
+++ Atividade8/src/calculadora/calculadora.js
@@ -1,5 +1,25 @@
-function somar(n1,n2){
-    return n1+ n2;
-}
-
-module //falta bastante coisa ..............
+exports.form = (req, res) => {
+    res.render('form');
+  };
+  
+  exports.calcular = (req, res) => {
+    const { numA, numB, operacao } = req.body;
+    let resultado;
+  
+    switch (operacao) {
+      case '+':
+        resultado = parseFloat(numA) + parseFloat(numB);
+        break;
+      case '-':
+        resultado = parseFloat(numA) - parseFloat(numB);
+        break;
+      case '*':
+        resultado = parseFloat(numA) * parseFloat(numB);
+        break;
+      case '/':
+        resultado = parseFloat(numA) / parseFloat(numB);
+        break;
+    }
+  
+    res.render('resultado', { resultado });
+  };
